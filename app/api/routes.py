817f61ea22843c112fa82301c4b937f201c2237d
slidebrass <slidebrass@gmail.com>
2023-12-04 12:09:13 -0600

    first commit

---
+++ app/api/routes.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Car, car_schema, cars_schema
+
+api = Blueprint('api', __name__, url_prefix='/api')
+
+@api.route('/cars', methods = ['POST'])
+@token_required
+def create_car(current_user_token):
+    prod_date = request.json['prod_date']
+    make = request.json['make']
+    model = request.json['model']
+    color = request.json['color']
+    user_token = current_user_token.token
+
+    print(f'BIG TESTER: {current_user_token.token}')
+
+    car = Car(prod_date, make, model, color, user_token = user_token)
+
+    db.session.add(car)
+    db.session.commit()
+
+    response = car_schema.dump(car)
+    return jsonify(response)
+
+@api.route('/cars', methods = ['GET'])
+@token_required
+def get_car(current_user_token):
+    a_user = current_user_token.token
+    cars = Car.query.filter_by(user_token = a_user).all()
+    response = cars_schema.dump(cars)
+    return jsonify(response)
+
+@api.route('/cars/<id>', methods = ['GET'])
+@token_required
+def get_single_car(current_user_token, id):
+    car = Car.query.get(id)
+    response = car_schema.dump(car)
+    return jsonify(response)
+
+@api.route('/cars/<id>', methods = ['POST', 'PUT'])
+@token_required
+def update_car(current_user_token, id):
+    car = Car.query.get(id)
+    car.prod_date = request.json['prod_date']
+    car.make = request.json['make']
+    car.model = request.json['model']
+    car.color = request.json['color']
+    car.user_token = current_user_token.token
+
+    db.session.commit()
+    response = car_schema.dump(car)
+    return jsonify(response)
+
+@api.route('/cars/<id>', methods = ['DELETE'])
+@token_required
+def delete_car(current_user_token, id):
+    car = Car.query.get(id)
+    db.session.delete(car)
+    db.session.commit()
+    response = car_schema.dump(car)
+    return jsonify(response)
